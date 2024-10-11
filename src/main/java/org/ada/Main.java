32db269ea82df7c14ed61ebed9bd0f5e2facfd9a
Adailson <adailson65@gmail.com>
2024-10-11 17:14:23 -0300

    first commit

---
+++ src/main/java/org/ada/Main.java
@@ -0,0 +1,9 @@
+package org.ada;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("-------------------------");
+        System.out.println("--------Executando-------");
+        System.out.println("-------------------------");
+    }
+}
---
+++ src/main/java/org/ada/ecommerce/TestaIntegracao.java
@@ -0,0 +1,31 @@
+package org.ada.ecommerce;
+
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+import java.util.Arrays;
+
+public class TestaIntegracao {
+
+    public static void main(String[] args) {
+        var user = "Gere 5 produtos";
+
+        var system = "Você é um gerador de produtos fictícios para um ecommerce e deve gerar apenas o nome dos produtos solicitados pelo usuário";
+
+        var service = new OpenAiService("chave");
+
+        var completionRequest = ChatCompletionRequest
+                .builder()
+                .model("gpt-4")
+                .messages(Arrays.asList(
+                        new ChatMessage(ChatMessageRole.USER.value(), user),
+                        new ChatMessage(ChatMessageRole.SYSTEM.value(), system)
+                ))
+                .build();
+        service
+                .createChatCompletion(completionRequest)
+                .getChoices()
+                .forEach(System.out::println);
+    }
+}
