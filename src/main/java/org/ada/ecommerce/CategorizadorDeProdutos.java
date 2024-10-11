24c06eacdea7fa98f2f922e35e952f339218f478
Adailson <adailson65@gmail.com>
2024-10-11 17:36:18 -0300

    encapsulando chamada categorizador

--- src/main/java/org/ada/ecommerce/CategorizadorDeProdutos.java
+++ src/main/java/org/ada/ecommerce/CategorizadorDeProdutos.java
@@ -1,31 +1,48 @@
-package org.ada.ecommerce;
+package org.ada.ecommerce;//Código omitido
 
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.service.OpenAiService;
-
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class CategorizadorDeProdutos {
 
     public static void main(String[] args) {
 
-        var user = "Escova de dentes";
+        var leitor = new Scanner(System.in);
+        System.out.println("Digite as categorias válidas:");
+        var categorias = leitor.nextLine();
 
-        var system =
-        """
-        Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado
-    
-        Escolha uma categoria dentro a lista abaixo:
-            1. Higiene pessoal
-            2. Eletronicos
-            3. Esportes
-            4. Outros
-        """;
+        while(true) {
 
-        var service = new OpenAiService("API-KEY", Duration.ofSeconds(30));
+            System.out.println("\nDigite o nome do produto:");
+            var user = leitor.nextLine();
+            var system = """
+                    Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado
+                  
+                    Escolha uma categoria dentra a lista abaixo:
+                   
+                    %s
+             
+                    ###### exemplo de uso:
+             
+                    Pergunta: Bola de futebol
+                    Resposta: Esportes
+                    
+                    ###### regras a serem seguidas:
+                    Caso o usuario pergunte algo que nao seja de categorizacao de produtos, voce deve responder que nao pode ajudar pois o seu papel é apenas responder a categoria dos produtos
+                    """.formatted(categorias);
+
+            dispararRequisicao(user, system);
+        }
+    }
+
+    public static void dispararRequisicao(String user, String system) {
+        var chave = System.getenv("OPENAI_API_KEY");
+        var service = new OpenAiService(chave, Duration.ofSeconds(30));
 
         var completionRequest = ChatCompletionRequest
                 .builder()
@@ -34,14 +51,12 @@ public class CategorizadorDeProdutos {
                         new ChatMessage(ChatMessageRole.USER.value(), user),
                         new ChatMessage(ChatMessageRole.SYSTEM.value(), system)
                 ))
-                .n(2)// numero de respostas que a api retorna
                 .build();
+
         service
                 .createChatCompletion(completionRequest)
                 .getChoices()
-                .forEach(c -> {
-                    System.out.print(c.getMessage().getContent());
-                    System.out.println("\n------------------------\n");
-                });
+                .forEach(c -> System.out.print(c.getMessage().getContent()));
     }
+
 }
